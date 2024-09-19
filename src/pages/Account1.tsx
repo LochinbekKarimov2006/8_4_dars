16e2ab24cce6b86441a99c4c878a5d8bb2e18576
LochinbekKarimov2006 <161306293+LochinbekKarimov2006@users.noreply.github.com>
2024-09-19 14:51:00 +0500

    .

--- src/pages/Account1.tsx
+++ src/pages/Account1.tsx
@@ -1,44 +1,105 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Account1() {
-    let data=useRef()
+  const [business, setBusiness] = useState<string>('');
+  const [type, setType] = useState<string>('');
+  const [addressLine1, setAddressLine1] = useState<string>('');
+  const [addressLine2, setAddressLine2] = useState<string>('');
+  const [city, setCity] = useState<string>('');
+  const [zip, setZip] = useState<string>('');
+
+  // Load data from localStorage when the component mounts
+  useEffect(() => {
+    const savedData = localStorage.getItem('accountData');
+    if (savedData) {
+      const { business, type, addressLine1, addressLine2, city, zip } = JSON.parse(savedData);
+      setBusiness(business);
+      setType(type);
+      setAddressLine1(addressLine1);
+      setAddressLine2(addressLine2);
+      setCity(city);
+      setZip(zip);
+    }
+  }, []);
+
+  // Save data to localStorage whenever any field changes
+  useEffect(() => {
+    const data = { business, type, addressLine1, addressLine2, city, zip };
+    localStorage.setItem('accountData', JSON.stringify(data));
+  }, [business, type, addressLine1, addressLine2, city, zip]);
+
   return (
     <div>
-        <div className=''>
-            <form className='div-3' action="">
-            <label className='input_text' htmlFor="">Business address
-            <select className='input' name="" id="" >
-                <option value="">Registered business address</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+      <div className=''>
+        <form className='div-3' action="">
+          <label className='input_text' htmlFor="">Business address
+            <select
+              value={business}
+              onChange={(e) => setBusiness(e.target.value)}
+              className='input'
+              name=""
+              id=""
+            >
+              <option value="oohtg3">Registered business address</option>
+              <option value="othoi35h"></option>
+              <option value="o5yihyi3"></option>
+              <option value="ptjh3j"></option>
+              <option value="35hhjp33j"></option>
             </select>
-                </label>
-                <label className='input_text' htmlFor="">Type
-            <select className='input' name="" id="" >
-                <option value="">Type of business</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+          </label>
+          <label className='input_text' htmlFor="">Type
+            <select
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+              className='input'
+              name=""
+              id=""
+            >
+              <option value="">Type of business</option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
+              <option value=""></option>
             </select>
-                </label>
-                <label className='input_text' htmlFor="">Address
-                <input className='input' type="text" placeholder='Address line 1' />
-                <input className='input' type="text" placeholder='Address line 2' />
-                <input className='input' type="text" placeholder='City' />
-                <input className='input' type="text" placeholder='Zip' />
-
-                </label>
-                <Link to="/1">
+          </label>
+          <label className='input_text' htmlFor="">Address
+            <input
+              className='input'
+              type="text"
+              placeholder='Address line 1'
+              value={addressLine1}
+              onChange={(e) => setAddressLine1(e.target.value)}
+            />
+            <input
+              className='input'
+              type="text"
+              placeholder='Address line 2'
+              value={addressLine2}
+              onChange={(e) => setAddressLine2(e.target.value)}
+            />
+            <input
+              className='input'
+              type="text"
+              placeholder='City'
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              className='input'
+              type="text"
+              placeholder='Zip'
+              value={zip}
+              onChange={(e) => setZip(e.target.value)}
+            />
+          </label>
+          <Link to="/1">
             <button className='button'>Continue »</button>
-            </Link>
-            </form>
-        </div>
+          </Link>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Account1
+export default Account1;
